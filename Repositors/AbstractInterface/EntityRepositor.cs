6c4f559a4b2c75f60b968640b2555de98f87ec57
Jonas Motiejauskas <jonasmotiejauskas@gmail.com>
2017-12-08 09:47:03 +0200

    Remade solution hierarcy. Again

--- Repositors/AbstractInterface/EntityRepositor.cs
+++ Repositors/AbstractInterface/EntityRepositor.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AbstractSchedule.Entities;
 
-namespace Repositors.AbstractInterface
+namespace AbstractRepositor.AbstractInterface
 {
     public interface IEntityRepositor
     {
